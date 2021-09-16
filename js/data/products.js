07bf19a648e6247a7cbe32431c644be63d9dc8fd
Yana <yanaegorova69.3@gmail.com>
2021-09-16 23:47:27 +0300

    +

---
+++ js/data/products.js
@@ -0,0 +1,151 @@
+export const products = [
+    {
+        id: 'product-1',
+        name: 'Craft Scissors',
+        price: 6.96,
+        image: './img/img1.png',
+        description: [
+            {
+                text: 'Contoured soft grip handles.',
+            },
+            {
+                text: 'Microtip blade allows for making precision cuts in tight spaces and for quick thread snips.',
+            },
+            {
+                text: 'Available in assorted colors, colors can\'t be chosen when ordered.',
+            },
+            {
+                text: 'Manufacturer\'s limited lifetime.',
+            },
+        ],
+        images: ['./img/img1-1.png', './img/img1-2.png'],
+    },
+    {
+        id: 'product-2',
+        name: 'Stand Up Bath Toy Bin',
+        price: 29.99,
+        image: './img/img2.png',
+        description: [
+            {
+                text: 'The 7.1 L Stand Up Bath Toy Bin with removable base is perfectly sized to fit all of baby’s bath toys.',
+            },
+            {
+                text: 'Unique hole pattern for quick and efficient drying, elevated tray helps catch any stray drips.',
+            },
+            {
+                text: 'Soft handles for scooping up toys from the tub.',
+            },
+            {
+                text: 'Non-slip, sturdy base keeps the Bin upright, base and bin separate for thorough cleaning.',
+            },
+            {
+                text: 'Dishwasher safe. PVC- and adhesive-free.',
+            },
+        ],
+        images: ['./img/img2-1.png', './img/img2-2.png'],
+    },
+    {
+        id: 'product-3',
+        name: 'Stadium Seat Chair',
+        price: 64.90,
+        image: './img/img3.png',
+        description: [
+            {
+                title: 'Cushioned Comfort',
+                text: 'With back support, arm rests, a wide seat, and plush cushioning, this stadium seat will provide ultimate comfort at any event- especially on bleachers, benches, or the ground!',
+            },
+            {
+                title: '6 Reclining Positions',
+                text: 'This folding chair has 6 different reclining options for a dialed in fit. Just push the seat back to adjust the chair position to an angle that is the most comfortable for you!',
+            },
+            {
+                title: 'Lightweight And Portable',
+                text: 'The lightweight cushioned bleacher chair folds flat between uses, and has two backpack style straps for easy portability and on-the-go seating.',
+            },
+            {
+                title: 'Multipurpose',
+                text: 'This seat has a skid and water resistant bottom so it can be used for a variety of events and activities. It is perfect to use at sporting events, outdoor camping, boating, concerts, the beach, or even just for extra seating at home.',
+            },
+            {
+                title: 'Product Details',
+                text: '17 inch wide seat, 15 inch long seat, 15 inch backrest, 3 inch armrest on each side. Materials: 420D PU coated polyester, high density foam filling, & steel tube.',
+            },
+        ],
+        images: ['./img/img3-1.png', './img/img3-2.png'],
+    },
+    {
+        id: 'product-4',
+        name: 'Digital Lever Handle Door Lock',
+        price: 98.90,
+        image: './img/img4.png',
+        description: [
+            {
+                text: 'Keyless Keypad Entry: Add up to 6 keypad users.',
+            },
+            {
+                text: 'Direct Replacement: No extra work on standard door prep.',
+            },
+            {
+                text: 'Easy Programming: Simple keypad programming procedure adds or deletes users in seconds.',
+            },
+            {
+                text: 'Glow in the Dark Keypad: Better visibility in the darK.',
+            },
+            {
+                text: 'Adjustable - Fits left and right handed doors.',
+            },
+        ],
+        images: ['./img/img4-1.png', './img/img4-2.png', './img/img4-3.png'],
+    },
+    {
+        id: 'product-5',
+        name: 'Chop Saw',
+        price: 119.98,
+        image: './img/img5.png',
+        description: [
+            {
+                text: '15 Amp, 3,800 RPM motor with replaceable brushes provides power and durability for the chop saw.',
+            },
+            {
+                text: 'Quick-release material clamp of the metal chop saw allows for easy material cutting and removal.',
+            },
+            {
+                text: 'Cutting fence with miter adjustment of the metal cutting saw allows up to 45 degree angles to be cut.',
+            },
+            {
+                text: 'Spark deflector provides safety and clear line of sight for cutting.',
+            },
+            {
+                text: 'Heavy duty steel base provides stability while cutting.',
+            },
+            {
+                text: 'Spindle lock for easy blade changes',
+            },
+        ],
+        images: ['./img/img5-1.png', './img/img5-2.png', './img/img5-3.png', './img/img5-4.png', './img/img5-5.png'],
+    },
+    {
+        id: 'product-6',
+        name: 'Leather Office Chair',
+        price: 148.00,
+        image: './img/img6.png',
+        description: [
+            {
+                text: 'Cushioning keeps you comfortable in this desk chair all day. This ergonomic office chair meets or exceeds industry standards for safety and durability.',
+            },
+            {
+                text: 'Support allows you to always find a comfortable position.',
+            },
+            {
+                text: 'Height adjustable arms allow you to adjust the height for upper body support. This computer desk chair\'s arms are also padded for comfort.',
+            },
+            {
+                text: 'Easy reach control paddles are located on the seat, making seat adjustments easier.',
+            },
+            {
+                text: 'Ergonomic touches make this office chair comfortable for your 8-5 workday. It features a tilt recline that allows for customized seat back comfort. The wheeled 5-star base provides stability and strength, holding users up to 275 lb.',
+            },
+        ],
+        images: ['./img/img6-1.png','./img/img6-2.png', './img/img6-3.png', './img/img6-4.png', './img/img6-5.png', './img/img6-6.png', './img/img6-7.png', './img/img6-8.png'],
+    },
+]
